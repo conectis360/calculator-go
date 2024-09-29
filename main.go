466e72d2abc91f0bf81e1aaaead6e5237503e835
James Garcia <james.ramos@gentecontrole.com.br>
2024-09-29 20:34:50 -0300

    Refactor: Modules, Code Reusability and messing with Error Handling in GO!

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -10,6 +11,8 @@ func main() {
 	var number1 float64
 	var number2 float64
 	var result float64
+	var numbersToOperate int
+	arrayOfNumbers := []float64{}
 
 	fmt.Println("Qual expressão básica (até 2 números) você deseja executar?")
 	fmt.Println("Digite 1 para somar")
@@ -20,23 +23,18 @@ func main() {
 
 	switch operacao {
 	case 1:
-		number1 = receiveNumbers()
-		number2 = receiveNumbers()
-		result = sum(number1, number2)
-		fmt.Println(result)
+		numbersToOperate = howManyNumbers()
+		fmt.Println(numbersToOperate)
+		arrayOfNumbers = repeatReceiver(numbersToOperate)
+
+		fmt.Println(sum(arrayOfNumbers))
 	case 2:
-		number1 = receiveNumbers()
-		number2 = receiveNumbers()
 		result = minus(number1, number2)
 		fmt.Println(result)
 	case 3:
-		number1 = receiveNumbers()
-		number2 = receiveNumbers()
 		result = multiply(number1, number2)
 		fmt.Println(result)
 	case 4:
-		number1 = receiveNumbers()
-		number2 = receiveNumbers()
 		result = divide(number1, number2)
 		fmt.Println(result)
 	default:
@@ -44,15 +42,44 @@ func main() {
 	}
 }
 
-func receiveNumbers() float64 {
-	var number float64
-	fmt.Println("Digite o valor?")
+func howManyNumbers() int {
+	var number int
+	fmt.Println("Digite com quantos algarismos você gostaria de calcular?")
 	fmt.Scanln(&number)
 	return number
 }
 
-func sum(number1 float64, number2 float64) float64 {
-	return number1 + number2
+func repeatReceiver(quantity int) []float64 {
+	var number int = 0
+	var number1 float64
+	var erro error
+	arrayOfNumbers := []float64{}
+	for number <= quantity {
+		number1, erro = receiveNumbers()
+		if erro == nil {
+			arrayOfNumbers = append(arrayOfNumbers, number1)
+		}
+		number++
+	}
+	return arrayOfNumbers
+}
+
+func receiveNumbers() (float64, error) {
+	var number float64
+	fmt.Println("Digite o valor?")
+	value, err := fmt.Scanln(&number) // Check if input parsing fails
+	if err != nil {
+		return 0, errors.New("math: digite um numero")
+	}
+	return number, nil
+}
+
+func sum(arrayOfNumbers []float64) float64 {
+	var number float64
+	for i := range len(arrayOfNumbers) {
+		number += arrayOfNumbers[i]
+	}
+	return number
 }
 
 func minus(number1 float64, number2 float64) float64 {
