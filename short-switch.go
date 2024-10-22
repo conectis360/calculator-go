4f0750db5a5a7e3f37de17b3a14f43acc720f4df
James Garcia <james.ramos@gentecontrole.com.br>
2024-10-21 23:57:54 -0300

    feat: short-switch

--- short-switch.go
+++ short-switch.go
@@ -1,19 +1,19 @@
 package main
 
-/*
 import (
 	"fmt"
 	"math/rand"
 )
 
 func main() {
-	if num := rand.Intn(3); num == 0 {
+	switch num := rand.Intn(10); num {
+	case 0:
 		fmt.Println("Space Adventures")
-	} else if num == 1 {
+	case 1:
 		fmt.Println("SpaceX")
-	} else {
+	case 2:
 		fmt.Println("Virgin Galactic")
+	default:
+		fmt.Println("Random spaceline #", num)
 	}
 }
-
-*/
