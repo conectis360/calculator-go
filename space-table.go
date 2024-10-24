1494ccea4c35fd348fc21fdbab956959e90efedf
James Garcia <james.ramos@gentecontrole.com.br>
2024-10-24 00:00:11 -0300

    feat: const trips, spacelines, for with values and print

--- space-table.go
+++ space-table.go
@@ -6,6 +6,20 @@ import (
 )
 
 func main() {
-	var values int = rand.Intn(10)
-	fmt.Print(values)
+	const tripTypeRound = "Round-trip"
+	const tripTypeOne = "One-way"
+
+	const virginGalatic = "Virgin Galactic"
+	const spaceX = "SpaceX"
+	const adventures = "Space Adventures"
+
+	fmt.Println(" Spaceline", "           ", "Days ", " Trip Type ", " Price")
+	fmt.Println(" ===============================================")
+	for x := 0; x <= 1; x++ {
+		var values int = rand.Intn(100)
+		var days int = rand.Intn(30) + 1
+		fmt.Println(" | ", virginGalatic, " | ", days, " | ", tripTypeOne, " | ", values, " | ")
+		fmt.Println(" | ", spaceX, "         | ", days, " | ", tripTypeOne, " | ", values, " | ")
+		fmt.Println(" | ", adventures, "    | ", days, " | ", tripTypeOne, " | ", values, " | ")
+	}
 }
