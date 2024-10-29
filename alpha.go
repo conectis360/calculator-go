df1396ff68437f51146602452c7b24c3b1721f68
James Garcia <james.ramos@gentecontrole.com.br>
2024-10-28 22:56:24 -0300

    feat: constants, Big, some calculations with big numbers

---
+++ alpha.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+import "fmt"
+
+func main() {
+	const lightSpeed = 299792 //km/s
+	const secondPerDay = 86400
+
+	var distance int64 = 41.3e12
+	fmt.Println("Alpha Centauri is: ", distance, "km away.")
+
+	days := distance / lightSpeed / secondPerDay
+	fmt.Println("That is", days, "days of t ravel at light speed.")
+}
+*/
--- piggy2.go
+++ piggy2.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import "fmt"
 
 func main() {
@@ -16,3 +17,4 @@ func main() {
 		cents += 20
 	}
 }
+*/
