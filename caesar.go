5d272dcf7bd1accdeb1bbbb33eac5c1d4f19988c
James Garcia <james.ramos@gentecontrole.com.br>
2024-10-31 23:10:15 -0300

    feat: strings manipulations, runes, utf-8, translations

---
+++ caesar.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+import (
+	"fmt"
+)
+
+func main() {
+	c := 'a'
+	c = c + 3
+	fmt.Printf("%c", c)
+
+	if c > 'z' {
+		c = c - 26
+		fmt.Println(c)
+	}
+
+	c = 'g'
+	c = c - 'a' + 'A'
+	fmt.Printf("%c", c)
+
+	message := "uv vagreangvbany fcnpr fgngvba"
+	fmt.Println(len(message))
+}
+*/
