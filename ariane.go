81dd99b13d89ae1fd4d1e718519f2ea0f0cb00ab
James Garcia <james.ramos@gentecontrole.com.br>
2024-11-01 23:45:07 -0300

    feat: more conversions

---
+++ ariane.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var bh float64 = 32767
+	var h = int16(bh)
+	fmt.Println(h)
+
+	v := 42
+	if v >= 0 && v <= math.MaxUint8 {
+		v8 := uint8(v)
+		fmt.Println("converted:", v8)
+	}
+}
+*/
--- mars-age.go
+++ mars-age.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
 	"fmt"
 )
@@ -13,3 +14,4 @@ func main() {
 	fmt.Println("I am", marsAge, "years old on Mars.")
 	fmt.Println(int(earthDays))
 }
+*/
