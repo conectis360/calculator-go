ba0023aca948ad54f64935cec2bf6400b1ff89f4
James Garcia <conectis360@gmail.com>
2024-11-04 23:28:15 -0300

    feat: converting bool to string using sprintf

--- itoa.go
+++ itoa.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
 	"fmt"
 	"strconv"
@@ -9,4 +10,9 @@ func main() {
 	countdown := 10
 	str := "Launch in T minus " + strconv.Itoa(countdown) + " seconds."
 	fmt.Println(str)
+
+	countdown = 9
+	str = fmt.Sprintf("Launch in T minus %v seconds.", countdown)
+	fmt.Println(str)
 }
+*/
