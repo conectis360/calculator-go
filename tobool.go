37c5db3ef9295b1d4028f116a833de793e376a33
James Garcia <conectis360@gmail.com>
2024-11-04 23:31:30 -0300

    feat: using if statement

--- tobool.go
+++ tobool.go
@@ -7,4 +7,13 @@ func main() {
 
 	launch := (yesNo == "yes")
 	fmt.Println("Ready for launch: ", launch)
+
+	launch = true
+	var oneZero int
+	if launch {
+		oneZero = 1
+	} else {
+		oneZero = 0
+	}
+	fmt.Println("Ready for launch:", oneZero)
 }
